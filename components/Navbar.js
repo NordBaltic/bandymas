ca066b8700a9fdb2f90a876ece6864c1bfa87324
NordBalticum STAFF <info@litexus.lt>
2025-03-03 15:49:17 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
-import styles from "../styles/globals.css"; // Importuojame globalius stilius
+import styles from "../styles/Navbar.module.css"; // Vietoj `globals.css`, naudokime atskirą CSS modulį
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
-            <div className="logo">
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
                 <Link href="/">
-                    <span className="brand">NordBalticum</span>
+                    <span className={styles.brand}>NordBalticum</span>
                 </Link>
             </div>
-            <div className="navbar-links">
-                <Link href="/dashboard" className="nav-link">Dashboard</Link>
-                <Link href="/stake" className="nav-link">Stake</Link>
-                <Link href="/donate" className="nav-link">Donate</Link>
-                <Link href="/profile" className="nav-link">Profile</Link>
+            <div className={styles.navbarLinks}>
+                <Link href="/dashboard" className={styles.navLink}>Dashboard</Link>
+                <Link href="/stake" className={styles.navLink}>Stake</Link>
+                <Link href="/donate" className={styles.navLink}>Donate</Link>
+                <Link href="/profile" className={styles.navLink}>Profile</Link>
             </div>
         </nav>
     );

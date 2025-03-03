21b226537a492654522836f9cf22a17541c2f7cd
NordBalticum STAFF <info@litexus.lt>
2025-03-03 06:14:41 +0200

    Create SwapWidget.js

---
+++ swap/SwapWidget.js
@@ -0,0 +1,12 @@
+export default function SwapWidget() {
+    return (
+        <div className="swap-widget">
+            <iframe
+                src="https://app.1inch.io/#/56/swap/BNB/USDT"
+                width="100%"
+                height="600px"
+                style={{ border: "none", borderRadius: "12px" }}
+            />
+        </div>
+    );
+}

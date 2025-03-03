cfa56adaa1ce2d31553dd01822c762cf209bf700
NordBalticum STAFF <info@litexus.lt>
2025-03-03 14:19:56 +0200

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../loginsystem/AuthProvider";
 import { supabase } from "../lib/supabaseClient";
 import toast from "react-hot-toast";
-import "../styles/profile.css"; // ✅ Importuojamas premium dizainas
 
 export default function Profile() {
     const { user, logout } = useAuth();

8e7da21242ce50cadccb79e6966e7c5351f8d831
NordBalticum STAFF <info@litexus.lt>
2025-03-03 07:22:29 +0200

    Update AdminDashboard.js

--- Admin/AdminDashboard.js
+++ Admin/AdminDashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import toast from "react-hot-toast";
-import "./AdminDashboard.css";
+import "./Admin.css";
 
 export default function AdminDashboard() {
     const [stats, setStats] = useState({

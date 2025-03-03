5d94cdabae963cb221ef527759348348a473b01c
NordBalticum STAFF <info@litexus.lt>
2025-03-03 07:22:59 +0200

    Update AdminTransactions.js

--- Admin/AdminTransactions.js
+++ Admin/AdminTransactions.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import toast from "react-hot-toast";
-import "./AdminTransactions.css";
+import "./Admin.css";
 
 export default function AdminTransactions() {
     const [transactions, setTransactions] = useState([]);
